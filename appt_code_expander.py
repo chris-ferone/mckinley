8d0fc3277984a70afc2f6565cf257ffede67bb33
Chris Ferone <chrisferone@gmail.com>
2021-06-17 08:24:05 -0400

    intermediate commit. Can parse code list and dictionary correctly using pandas excel import

--- appt_code_expander.py
+++ appt_code_expander.py
@@ -1,30 +1,60 @@
 # Reading an excel file using Python
-import xlrd
+import pandas as pd
+import math
+df = pd.read_excel(r'C:\Users\Chris\Downloads\UnitTypev32.xlsx', header=5, sheet_name='AC', usecols="A,AC:AD", nrows=100)
+#print("start")
+#print(df)
+#print("end")
+#print(df.iloc[9:54, 1:3])
+#print(pd.Series(df.iloc[9:19, 2],index=df.iloc[9:19, 3]).to_dict())
+#print(xcl_dict)
+#AC16
+#dict =
+#print("column names")
+#print(df.columns.values)
+#['Type' 'Unnamed: 28' 'Count.1']
+#print(df['Unnamed: 28'])
 
-# Give the location of the file
-loc = ("sample.xls")
-
-# To open Workbook
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
-
-# For row 0 and column 0
-print(sheet.cell_value(5, 4))
-
-Dict = {'G': '1st Floor', 'T': 'Top', 'F': 'Furnished','P': 'Pool View'}
-#print(Dict)
-
-Codes = ['GF', 'PFT']
-
+codes = df.iloc[:, 0].tolist()
+clean_codes=[]
+for i in codes:
+    if str(i) == 'nan':
+        break
+    else:
+        clean_codes.append(i)
+#print(codes)
+print("codes")
+print(clean_codes)
+print("")
+dict_keys = df.iloc[9:54, 1].tolist()
+dict_values = df.iloc[9:54, 2].tolist()
+dict={}
+for i in range(len(dict_keys)):
+    if type(dict_keys[i]) != str:
+        break
+    else:
+        dict[dict_keys[i]] = dict_values[i]
+print("Dictionary")
+print(dict)
+# dict = {'G': '1st Floor', 'T': 'Top', 'F': 'Furnished','P': 'Pool View'}
+# #print(dict)
+#
+# codes = ['GF', 'PFT']
+#
 # Iterate over list of codes
-for i in range(0,len(Codes)):
-    code_len = len(Codes[i])
+for i in range(0,len(codes)):
+    code_len = len(codes[i])
     #print("here")
     Description = ""
     # Iterate over code
-    for j in Codes[i]:
-        #print(j)
-        #print(Dict[j])
-        Description =  Description + ", " + Dict[j]
+    print(codes[i])
+    first_run = True
+    for j in codes[i]:
+        print(j)
+        print(dict[j])
+        if not first_run:
+            Description =  Description + ", " + dict[j]
+        else:
+            first_run = False
     Description = Description[2:]
     print(Description)
